ef3993b1b4090ef5448a4530f1430ace1bf9bc60
Christopher Rasch-Olsen Raa <christopher@stix.no>
2014-07-05 16:07:46 +0200

    Fix for change in git status output format

--- scripts/06-bash_prompt.sh
+++ scripts/06-bash_prompt.sh
@@ -1,6 +1,6 @@
         RED="\[\033[0;31m\]"
      YELLOW="\[\033[0;33m\]"
-		   GREEN="\[\033[0;32m\]"
+      GREEN="\[\033[0;32m\]"
        BLUE="\[\033[0;34m\]"
   LIGHT_RED="\[\033[1;31m\]"
 LIGHT_GREEN="\[\033[1;32m\]"
@@ -12,13 +12,14 @@ function parse_git_branch {
 
 	git rev-parse --git-dir &> /dev/null
 	git_status="$(git status 2> /dev/null)"
-	branch_pattern="^# On branch ([^${IFS}]*)"
-	remote_pattern="# Your branch is (.*) of"
-	diverge_pattern="# Your branch and (.*) have diverged"
+	branch_pattern="On branch ([^${IFS}]*)"
+	remote_pattern="Your branch is (.*) of"
+	diverge_pattern="Your branch and (.*) have diverged"
 	state=" ${GREEN}✓"
 	if [[ ! ${git_status} =~ "working directory clean" ]]; then
 		state=" ${RED}✘"
 	fi
+
 	# add an else if or two here if you want to get more specific
 	if [[ ${git_status} =~ ${remote_pattern} ]]; then
 		if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
