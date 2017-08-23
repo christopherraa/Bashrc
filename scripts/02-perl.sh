5a41dbf98a6855d3bed653812b8068501cfdd396
Christopher Rasch-Olsen Raa <christopher@stix.no>
2017-08-23 16:42:16 +0200

    Some additional functions for my use of plenv
    Now exporting two function, `perl_clear_environment` and
    `perl_libdir`. The second of these let you specify a directory in
    which perl should look for modules. The default is `./local`. In
    addition to that it adds `lib/` and `t/lib` to $PERL5LIB if those
    directories exist. This mean I can now run the following when opening
    a new terminal and have my nicely isolated enviroment set up for emacs
    and other applications that I use with / for Perl development.
    
      $ perl_libdir
    	$ emacs

--- scripts/02-perl.sh
+++ scripts/02-perl.sh
@@ -1,2 +1,24 @@
 #export PERL_UNICODE=SDL
 export PERLDOC="-MPod::Text::Color::Delight"
+
+function perl_clear_environment {
+	unset PERL5LIB
+	unset PERL_MB_OPT
+	unset PERL_LOCAL_LIB_ROOT
+	unset PERL_MM_OPT
+}
+
+function perl_libdir {
+	perl_clear_environment
+	libdir=$1
+	if [ -z "$libdir" ]; then
+		libdir=local
+	fi
+	eval "$( perl -Mlocal::lib=$libdir - )"
+	if test -d lib; then
+		export PERL5LIB="$PWD/lib:$PERL5LIB"
+	fi
+	if test -d t/lib; then
+		export PERL5LIB="$PWD/t/lib:$PERL5LIB"
+	fi
+}
